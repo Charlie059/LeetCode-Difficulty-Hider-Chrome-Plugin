343257399a607a4fda1466c34c420c48d77b52cc
your_username <pad128g@icloud.com>
2024-01-31 11:00:39 -0500

    feat: add US version

--- content.js
+++ content.js
@@ -1,19 +1,20 @@
 function hideDifficulty() {
-    const difficulties = ['简单', '中等', '困难'];
+  const difficulties = ["简单", "中等", "困难", "Easy", "Medium", "Hard"];
 
-    const elements = Array.from(document.querySelectorAll('span, div'));
+  const elements = Array.from(document.querySelectorAll("span, div"));
 
-    elements.forEach(el => {
-        if (difficulties.some(difficulty => el.innerText === difficulty)) {
-            el.style.display = 'none';
-        }
-    });
+  elements.forEach((el) => {
+    if (difficulties.some((difficulty) => el.innerText === difficulty)) {
+      el.style.display = "none";
+      console.log("Hiding difficulty: ", el.innerText);
+    }
+  });
 }
 
 hideDifficulty();
 
 const observer = new MutationObserver(hideDifficulty);
 observer.observe(document.body, {
-    childList: true,
-    subtree: true
+  childList: true,
+  subtree: true,
 });
